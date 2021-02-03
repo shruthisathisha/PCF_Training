84babe4bab94d0f84d2f210ea730dbf45a4d27c0
Ubuntu <ubuntu@ip-172-31-80-150.ec2.internal>
2021-02-03 14:46:06 +0000

    commiting

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,19 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    String hello;
+
+    public WelcomeController(@Value("${welcome.message}") String message){
+        hello=message;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return hello;
+    }
+}
